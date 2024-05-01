59c84f009ee4ecda25c675dc5699f8c336ea2e15
Shreya Pallan <shreyapallan@Shreyas-MacBook-Pro.local>
2024-05-01 17:27:10 +0530

    Create Essential Feed

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Shreya Pallan on 01/05/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
