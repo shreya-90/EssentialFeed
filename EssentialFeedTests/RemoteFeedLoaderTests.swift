0827f477a99aec4e9bdd4efcc6a6ef34a90b3507
Shreya Pallan <shreyapallan@Shreyas-MacBook-Pro.local>
2024-05-02 18:26:58 +0530

    Test for calling load more than once

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -16,7 +16,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
 
-    func test_init_requestsDataFromUrl() {
+    func test_load_requestsDataFromUrl() {
         let url = URL(string: "https://a-given-url.com")!
        
         let (sut, client) = makeSUT(url: url)
@@ -26,6 +26,19 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    func test_loadTwice_requestsDataFromUrlTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+       
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url,url])
+        
+    }
+    
+    
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
@@ -34,9 +47,11 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPCLient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
