37f590318044b2d04d2c93661b4833d395ed9ab9
Shreya Pallan <shreyapallan@Shreyas-MacBook-Pro.local>
2024-05-07 13:30:09 +0530

    Move Decodable logic to a new private item struct to decouple the Feed Feature module from the API module

--- EssentialFeed/RemoteFeedLoader.swift
+++ EssentialFeed/RemoteFeedLoader.swift
@@ -41,9 +41,9 @@ public final class RemoteFeedLoader {
         client.get(from: self.url) { result in
             
             switch result {
-            case let .success(data, _ ):
-                if let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items))
+            case let .success(data, response ):
+                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
+                    completion(.success(root.items.map{ $0.item }))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -56,5 +56,19 @@ public final class RemoteFeedLoader {
 
 
 private struct Root: Decodable {
-    let items: [FeedItem]
+    let items: [Item]
 }
+
+private struct Item: Decodable {
+     let id: UUID
+     let description: String?
+     let location: String?
+     let image: URL
+    
+    var item: FeedItem {
+        return FeedItem(id: id, description: description, location: location, imageURL: image)
+    }
+}
+
+
+
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -56,7 +56,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500].enumerated()
         samples.forEach { index, code in
             expect(sut, toCompleteWithResult: .failure(.invalidData)) {
-                client.complete(withStatusCode: code, index: index)
+                let json = makeItemsJSON([])
+                client.complete(withStatusCode: code, data: json, index: index)
             }
         }
     }
@@ -157,7 +158,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, data: Data = Data(), index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data, index: Int = 0) {
             
             let response = HTTPURLResponse(url: requestedURLs[index], 
                                            statusCode: code,
