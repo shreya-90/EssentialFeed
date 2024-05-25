6ee83452a394bad20a0473de88d71a69466fc5e0
Shreya Pallan <shreyapallan@Shreyas-MacBook-Pro.local>
2024-05-25 12:38:53 +0530

    Move LocalFeedLoader (and FeedStore collaborator) to its own file in production

---
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,50 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Shreya Pallan on 25/05/24.
+//
+
+import Foundation
+
+
+public final class LocalFeedLoader {
+    
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.deleteCacheFeed { [weak self] error in
+            
+            guard let self = self else { return }
+            
+            if let cacheDeletionError = error  {
+                completion(cacheDeletionError)
+            } else {
+                
+                self.cacheItems(items, with: completion)
+            }
+        }
+    }
+    
+    private func cacheItems(_ items: [FeedItem], with completion: @escaping (Error?) -> Void){
+        store.insert(items, timestamp: currentDate(), completion: { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        })
+    }
+}
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCacheFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+
+}
