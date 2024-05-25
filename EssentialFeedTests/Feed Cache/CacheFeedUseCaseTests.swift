5e07246fe4205f1b253d5bffd516a2d098b23bb1
Shreya Pallan <shreyapallan@Shreyas-MacBook-Pro.local>
2024-05-25 12:32:47 +0530

    Add cahce insertion inside a helper function to make logic inside closure callbacks easier to follow

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -26,12 +26,16 @@ class LocalFeedLoader {
             if let cacheDeletionError = error  {
                 completion(cacheDeletionError)
             } else {
-                store.insert(items, timestamp: self.currentDate(), completion: { [weak self] error in
-                    guard self != nil else { return }
-                    completion(error)
-                })
+                self.cacheItems(items, with: completion)
             }
         }
+    }
+    
+    private func cacheItems(_ items: [FeedItem], with completion: @escaping (Error?) -> Void){
+        store.insert(items, timestamp: currentDate(), completion: { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        })
     }
 }
 
