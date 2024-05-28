ea2d14c9777ebbf9cbc8d2dc8e83b004ddf450f8
Shreya Pallan <shreyapallan@Shreyas-MacBook-Pro.local>
2024-05-28 17:14:21 +0530

    Adding store URL as adependancy to the CodableFeedStore

--- EssentialFeedTests/Feed Cache/CoadableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoadableFeedStoreTests.swift
@@ -37,7 +37,11 @@ class CodableFeedStore {
         }
     }
     
-    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    private let storeURL: URL
+    
+    init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
     
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         guard let data = try? Data(contentsOf: storeURL) else {
@@ -148,8 +152,10 @@ final class CoadableFeedStoreTests: XCTestCase {
     
         //MARK: - Helpers
     func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-        let sut = CodableFeedStore()
-        trackForMemoryLeaks(sut, file: file, line: line)
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        let sut = CodableFeedStore(storeURL: storeURL)
+        trackForMemoryLeaks(sut, file: file, line: line
+        )
         return sut
     }
 
