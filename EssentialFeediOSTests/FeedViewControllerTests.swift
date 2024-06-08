ff35bfba47a762e28dc4b470c77cd51f3ca1f25e
Shreya Pallan <shreyapallan@Shreyas-MacBook-Pro.local>
2024-06-08 17:33:39 +0530

    Extract UIButton test helper to a separate file

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -451,13 +451,5 @@ private class FakeRefreshControl: UIRefreshControl {
     
 }
 
-private extension UIButton {
-    func simulateTap() {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
-    }
-}
+
 
