f3b32d8f4d29b6190bf85c8425b8b748ca1df7ac
Shreya Pallan <shreyapallan@Shreyas-MacBook-Pro.local>
2024-06-11 15:04:43 +0530

    Decouple FeedImageViewModel from UIKit by creating a transformation closure that converts an image 'Data' value into a generic 'Image' type. When composing with a UIKit user interface, we inject a closure to transform the image 'Data' into 'UIImage'.

--- EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -8,64 +8,11 @@
 import UIKit
 import EssentialFeed
 
-final class FeedImageViewModel {
-    typealias Observer<T> = (T) -> Void
-    
-    private var task: FeedImageDataLoaderTask?
-    private let model: FeedImage
-    private let imageLoader: FeedImageDataLoader
-    
-    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
-        self.model = model
-        self.imageLoader = imageLoader
-    }
-    
-    var description: String? {
-        return model.description
-    }
-    
-    var location: String?  {
-        return model.location
-    }
-    
-    var hasLocation: Bool {
-        return location != nil
-    }
-    
-    var onImageLoad: Observer<UIImage>?
-    var onImageLoadingStateChange: Observer<Bool>?
-    var onShouldRetryImageLoadStateChange: Observer<Bool>?
-    
-    func loadImageData() {
-        onImageLoadingStateChange?(true)
-        onShouldRetryImageLoadStateChange?(false)
-        
-        self.task = self.imageLoader.loadImageData(from: model.url) { [weak self] result in
-            self?.handle(result)
-        }
-    }
-    
-    func handle(_ result: FeedImageDataLoader.Result) {
-        if let image = (try? result.get()).flatMap(UIImage.init) {
-                onImageLoad?(image)
-        } else {
-            onShouldRetryImageLoadStateChange?(true)
-        }
-        onImageLoadingStateChange?(false)
-    }
-    
-    func cancelImageDataLoad() {
-        task?.cancel()
-        task = nil
-    }
-    
-}
-
 final class FeedImageCellController {
     
-    private let viewModel: FeedImageViewModel
+    private let viewModel: FeedImageViewModel<UIImage>
     
-    init(viewModel: FeedImageViewModel) {
+    init(viewModel: FeedImageViewModel<UIImage>) {
         self.viewModel = viewModel
     }
     
