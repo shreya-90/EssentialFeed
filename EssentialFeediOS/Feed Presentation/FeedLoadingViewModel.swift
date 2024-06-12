debb38ed17283c90361bb9bdc11e688d1d3bc47c
Shreya Pallan <shreyapallan@Shreyas-MacBook-Pro.local>
2024-06-12 14:03:12 +0530

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Shreya Pallan on 12/06/24.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
